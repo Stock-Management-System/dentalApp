34a16cf867e2550edf929906e47daa0071fc2487
selman <gunduzgece546@gmail.com>
2022-11-11 23:21:42 +0300

    sidebar

--- src/components/polyclinic/Sidebar.jsx
+++ src/components/polyclinic/Sidebar.jsx
@@ -7,20 +7,22 @@ import { FaChessBoard } from 'react-icons/fa';
 import { FaHospitalUser } from 'react-icons/fa';
 import { FaTooth } from 'react-icons/fa';
 import { ImCalendar } from 'react-icons/im';
-
+import {BiPlusMedical} from 'react-icons/bi'
 const Sidebar = ({ open, setOpen }) => {
     return (
-        <div className="relative h-screen bg-indigo-600" style={{ width: open ? "220px" : "75px" }}>
+        <div className="relative h-screen bg-gradient-to-b from-blue1 to-blue2 text-white " style={{ width: open ? "220px" : "75px" }}>
             <div>
                 <MdArrowBackIosNew style={{ marginLeft: '-4.5px' }} className={open ? "absolute top-6 left-56 z-10 btn-primary rounded-r-full" : "absolute top-6 left-20 z-10 rotate-180 btn-primary rounded-l-full"} onClick={() => setOpen(!open)} />
-                <div className='h-16 flex justify-center items-center text-white'>
-                    <h1>
-                        Dentist
-                    </h1>
+                <div className='h-16 flex justify-center items-center text-indigo-500'>
+            <BiPlusMedical />
+            {/* font-size: 2.8rem;
+    font-weight: 700;
+    letter-spacing: -.5px; */}
+            <h1 className="fontSize-2.5 text-2.5">D<span style={{ display: !open &&'none' }}>ent<strong>rey</strong></span></h1>
                 </div>
                 <div>
                     {open ?
-                        <ul className="menu p-4 w-50 bg-indigo-600 text-white">
+                        <ul className="menu p-4 w-50  text-white">
                             <li><a><FaTooth />Kliniğim</a></li>
                             <li><a><ImCalendar /> Randevular</a></li>
                             <li><a><FaHospitalUser /> Doktorlar</a></li>
@@ -30,7 +32,7 @@ const Sidebar = ({ open, setOpen }) => {
                             <li><a><FiSettings /> Ayarlar</a></li>
                         </ul>
                         :
-                        <ul className="menu p-4 w-50 bg-indigo-600 text-white gap-2 py-5">
+                        <ul className="menu p-4 w-50 text-white gap-2 py-5">
                             <li><a><FaTooth /></a></li>
                             <li><a><ImCalendar /></a></li>
                             <li><a><FaHospitalUser /></a></li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     './src/**/*.{html,js}',
   ],
   theme: {
+    // colors: {
+    //   transparent: 'transparent',
+    //   current: 'currentColor',
+    //   'white': '#ffffff',
+    //   'purple': '#3f3cbb',
+    //   'midnight': '#121063',
+    //   'metal': '#565584',
+    //   'tahiti': '#3ab7bf',
+    //   'silver': '#ecebff',
+    //   'bubble-gum': '#ff77e9',
+    //   'bermuda': '#78dcca',
+    // },
+    colors: {
+      
+      'blue1': '#5616f5',
+      'blue2': '#7d28eb',
+      'white': '#ffffff',
+      'fuchsia': {
+        50:'#fdf4ff',
+        600:'#c026d3',
+      },
+    },
+    fontSize: {
+      '2.5': '2.5rem',
+      'base': '1rem',
+      'xl': '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    },
     screens: {
       'xs': '320px',
       // => @media (min-width: 640px) { ... }
@@ -23,11 +55,6 @@ module.exports = {
       '2xl': '1536px',
       // => @media (min-width: 1536px) { ... }
     }
-  },
-  colors: {
-
-    'blue1': '#5616f5',
-    'blue2': '#7d28eb',
   },
   plugins: [
     require("daisyui"),
