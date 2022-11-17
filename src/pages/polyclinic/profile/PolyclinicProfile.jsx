7e0fde4be8dfdcf78046584de2b8ab76527e8658
FatihG34 <fthgnydn34@gmail.com>
2022-11-17 21:17:14 +0300

    MiddleLink modified

--- src/pages/polyclinic/profile/PolyclinicProfile.jsx
+++ src/pages/polyclinic/profile/PolyclinicProfile.jsx
@@ -7,21 +7,21 @@ import Container from '../../../components/polyclinic/Container'
 import "./polyclinicProfile.scss"
 
 const PolyclinicProfile = () => {
-	return (
-		<>
-			<Layout />
-			
-    <div
-      className="absolute bg-container  mt-20 right-0 p-6 min-h-[calc(100vh-80px)]"
-      style={{ width: open ? 'calc(100% - 250px)' : 'calc(100% - 100px)' }}
-    >
-      <div className="bg-white">
-        <ClinicHeader />
+  const open = useSelector(state => state.open.value)
+  return (
+    <>
+      <Layout />
+
+      <div
+        className="absolute bg-container  mt-20 right-0 p-6 min-h-[calc(100vh-80px)]"
+        style={{ width: open ? 'calc(100% - 250px)' : 'calc(100% - 100px)' }}
+      >
+        <div className="bg-white">
+          <ClinicHeader />
+        </div>
         <MiddleLink />
       </div>
-    </div>
-
-  </>
+    </>
   )
 }
 
