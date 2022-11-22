9ee7efd0d590e5e0b1797e3ca17bfc0cba787516
omersb <omersaidbuldukk@gmail.com>
2022-11-22 19:39:46 +0300

    new doctor add pages

---
+++ src/components/polyclinic/doctors/NewDoctorAddPage.jsx
@@ -0,0 +1,470 @@
+import React from "react";
+import quadrant1 from "../../../assets/quadrant1.png";
+import quadrant2 from "../../../assets/quadrant2.png";
+import quadrant3 from "../../../assets/quadrant3.png";
+import users from "../../../assets/users.png";
+
+const NewDoctorAddPage = () => {
+	return (
+		<div className="py-3 px-5">
+			{/* <----- KİŞİSEL BİLGİLER -----> */}
+			<p className="text-xl font-semibold antialiased leading-loose">
+				Kişisel Bilgi
+			</p>
+			<div className="mt-5">
+				<div className="flex flex-row gap-3">
+					<div className="flex justify-center w-1/3 transition bg-container rounded-sm  cursor-pointer hover:border border-gray">
+						<img src={users} alt="users" />
+						{/* <p className="text-center">
+							Resmi buraya bırakın veya yüklemek için tıklayın.
+						</p> */}
+					</div>
+					<div className="form-control w-1/3">
+						<div className="form-control w-full">
+							<label className="label-text mb-1">ADI *</label>
+							<input
+								type="text"
+								placeholder="Type here"
+								className="input input-bordered w-full"
+							/>
+						</div>
+						<div className="form-control w-full mt-4">
+							<label className="label-text mb-1">E-POSTA *</label>
+							<input
+								type="email"
+								placeholder="****@*****.com"
+								className="input input-bordered w-full "
+							/>
+						</div>
+						<div className="form-control w-full mt-4">
+							<label className="label-text mb-1">CİNSİYET</label>
+							<select className="select select-bordered w-full">
+								<option disabled selected>
+									Bir cinsiyet seçim
+								</option>
+								<option>Bay</option>
+								<option>Bayan</option>
+							</select>
+						</div>
+						<div className="form-control w-full mt-4">
+							<label className="label-text mb-1">ŞİFRE</label>
+							<input
+								type="password"
+								placeholder="Type here"
+								className="input input-bordered w-full"
+							/>
+						</div>
+					</div>
+					<div className="form-control w-1/3">
+						<div className="form-control w-full">
+							<label className="label-text mb-1">SOYADI *</label>
+							<input
+								type="text"
+								placeholder="Type here"
+								className="input input-bordered w-full"
+							/>
+						</div>
+						<div className="form-control w-full mt-4">
+							<label className="label-text mb-1">TELEFON *</label>
+							<input
+								type="text"
+								className="input input-bordered w-full"
+								placeholder="Type here"
+							/>
+						</div>
+						<div className="form-control w-full mt-4">
+							<label className="label-text mb-1">DOĞUM TARİHİ</label>
+							<input
+								type="text"
+								placeholder="Type here"
+								className="input input-bordered w-full"
+							/>
+						</div>
+						<div className="form-control w-full mt-4">
+							<label className="label-text mb-1">ŞİFRE (TEKRAR)</label>
+							<input
+								type="password"
+								placeholder="Type here"
+								className="input input-bordered w-full"
+							/>
+						</div>
+					</div>
+				</div>
+				<div className="form-control w-full mt-5">
+					<label className="label-text mb-1">ADRES *</label>
+					<input
+						type="text"
+						placeholder="Type here"
+						className="input input-bordered w-full"
+					/>
+				</div>
+				<div className="flex flex-row gap-3 mt-4">
+					<div className="form-control w-1/3">
+						<label className="label-text mb-1">ÜLKE</label>
+						<input
+							type="text"
+							placeholder="Type here"
+							className="input input-bordered w-full"
+						/>
+					</div>
+					<div className="form-control w-1/3">
+						<label className="label-text mb-1">KENT</label>
+						<input
+							type="text"
+							placeholder="Type here"
+							className="input input-bordered w-full"
+						/>
+					</div>
+					<div className="form-control w-1/3">
+						<label className="label-text mb-1">POSTA KODU</label>
+						<input
+							type="number"
+							placeholder="Type here"
+							className="input input-bordered w-full"
+						/>
+					</div>
+				</div>
+				<button className="btn btn-active btn-sm rounded-full my-6 p-2 px-8 bg-blue1 hover:bg-blue1">
+					DOKTOR EKLE
+				</button>
+			</div>
+			{/* <----- MESLEKİ BİLGİLER -----> */}
+			<div>
+				<p className="text-xl font-semibold antialiased leading-loose">
+					Mesleki Bilgiler
+				</p>
+				<div className="flex flex-row gap-3 mt-3">
+					<div className="form-control w-2/4">
+						<label className="label-text mb-1">UZMANLIK</label>
+						<input
+							type="text"
+							placeholder="Type here"
+							className="input input-bordered w-full"
+						/>
+					</div>
+					<div className="form-control w-1/4">
+						<label className="label-text mb-1">DİPLOMA NO</label>
+						<input
+							type="text"
+							placeholder="Type here"
+							className="input input-bordered w-full"
+						/>
+					</div>
+					<div className="form-control w-1/4">
+						<label className="label-text mb-1">İŞE BAŞLAMA TARİHİ</label>
+						<input
+							type="number"
+							placeholder="Type here"
+							className="input input-bordered w-full"
+						/>
+					</div>
+				</div>
+				<div className="flex flex-row gap-3 mt-4">
+					<div className="form-control w-1/2">
+						<label className="label-text mb-1">TIP EĞİTİMİ</label>
+						<input
+							type="text"
+							placeholder="Type here"
+							className="input input-bordered w-full"
+						/>
+					</div>
+					<div className="form-control w-1/2">
+						<label className="label-text mb-1">DÖNEM</label>
+						<input
+							type="number"
+							placeholder="Type here"
+							className="input input-bordered w-full"
+						/>
+					</div>
+				</div>
+				<div className="form-control w-full mt-4">
+					<label className="label-text mb-1">NOTLAR *</label>
+					<input
+						type="text"
+						placeholder="Type here"
+						className="input input-bordered w-full"
+					/>
+				</div>
+				<button className="btn btn-active btn-sm rounded-full my-6 p-2 px-8 bg-blue1 hover:bg-blue1">
+					KAYDET
+				</button>
+			</div>
+			{/* <----- ÇALIŞMA GÜNLERİ -----> */}
+			<div>
+				<p className="text-xl font-semibold antialiased leading-loose">
+					Çalışma Günleri
+				</p>
+				<div className="overflow-x-auto pr-5">
+					<table className="table w-full">
+						{/* <!-- head --> */}
+						<thead className="bg-blue1 text-center">
+							<tr>
+								<th></th>
+								<th>PAZARTESİ</th>
+								<th>SALI</th>
+								<th>ÇARŞAMBA</th>
+								<th>PERŞEMBE</th>
+								<th>CUMA</th>
+								<th>CUMAERTESİ</th>
+								<th>PAZAR</th>
+							</tr>
+						</thead>
+						<tbody>
+							{/* <!-- row 1 --> */}
+							<tr>
+								<th>
+									<img src={quadrant1} alt="quadrant" />
+								</th>
+								<td>
+									<label className="swap text-sm">
+										<input type="checkbox" />
+										<div className="swap-on border rounded-full border-blue1 text-blue1 p-1 px-3">
+											09:00 - 12:00
+										</div>
+										<div className="swap-off border rounded-full border-gray text-gray p-1 px-3">
+											09:00 - 12:00
+										</div>
+									</label>
+								</td>
+								<td>
+									<label className="swap text-sm">
+										<input type="checkbox" />
+										<div className="swap-on border rounded-full border-blue1 text-blue1 p-1 px-3">
+											09:00 - 12:00
+										</div>
+										<div className="swap-off border rounded-full border-gray text-gray p-1 px-3">
+											09:00 - 12:00
+										</div>
+									</label>
+								</td>
+								<td>
+									<label className="swap text-sm">
+										<input type="checkbox" />
+										<div className="swap-on border rounded-full border-blue1 text-blue1 p-1 px-3">
+											09:00 - 12:00
+										</div>
+										<div className="swap-off border rounded-full border-gray text-gray p-1 px-3">
+											09:00 - 12:00
+										</div>
+									</label>
+								</td>
+								<td>
+									<label className="swap text-sm">
+										<input type="checkbox" />
+										<div className="swap-on border rounded-full border-blue1 text-blue1 p-1 px-3">
+											09:00 - 12:00
+										</div>
+										<div className="swap-off border rounded-full border-gray text-gray p-1 px-3">
+											09:00 - 12:00
+										</div>
+									</label>
+								</td>
+								<td>
+									<label className="swap text-sm">
+										<input type="checkbox" />
+										<div className="swap-on border rounded-full border-blue1 text-blue1 p-1 px-3">
+											09:00 - 12:00
+										</div>
+										<div className="swap-off border rounded-full border-gray text-gray p-1 px-3">
+											09:00 - 12:00
+										</div>
+									</label>
+								</td>
+								<td>
+									<label className="swap text-sm">
+										<input type="checkbox" />
+										<div className="swap-on border rounded-full border-blue1 text-blue1 p-1 px-3">
+											09:00 - 12:00
+										</div>
+										<div className="swap-off border rounded-full border-gray text-gray p-1 px-3">
+											09:00 - 12:00
+										</div>
+									</label>
+								</td>
+								<td>
+									<label className="swap text-sm">
+										<input type="checkbox" />
+										<div className="swap-on border rounded-full border-blue1 text-blue1 p-1 px-3">
+											09:00 - 12:00
+										</div>
+										<div className="swap-off border rounded-full border-gray text-gray p-1 px-3">
+											09:00 - 12:00
+										</div>
+									</label>
+								</td>
+							</tr>
+							{/* <!-- row 2 --> */}
+							<tr>
+								<th>
+									<img src={quadrant2} alt="quadrant" />
+								</th>
+								<td>
+									<label className="swap text-sm">
+										<input type="checkbox" />
+										<div className="swap-on border rounded-full border-blue1 text-blue1 p-1 px-3">
+											09:00 - 12:00
+										</div>
+										<div className="swap-off border rounded-full border-gray text-gray p-1 px-3">
+											09:00 - 12:00
+										</div>
+									</label>
+								</td>
+								<td>
+									<label className="swap text-sm">
+										<input type="checkbox" />
+										<div className="swap-on border rounded-full border-blue1 text-blue1 p-1 px-3">
+											09:00 - 12:00
+										</div>
+										<div className="swap-off border rounded-full border-gray text-gray p-1 px-3">
+											09:00 - 12:00
+										</div>
+									</label>
+								</td>
+								<td>
+									<label className="swap text-sm">
+										<input type="checkbox" />
+										<div className="swap-on border rounded-full border-blue1 text-blue1 p-1 px-3">
+											09:00 - 12:00
+										</div>
+										<div className="swap-off border rounded-full border-gray text-gray p-1 px-3">
+											09:00 - 12:00
+										</div>
+									</label>
+								</td>
+								<td>
+									<label className="swap text-sm">
+										<input type="checkbox" />
+										<div className="swap-on border rounded-full border-blue1 text-blue1 p-1 px-3">
+											09:00 - 12:00
+										</div>
+										<div className="swap-off border rounded-full border-gray text-gray p-1 px-3">
+											09:00 - 12:00
+										</div>
+									</label>
+								</td>
+								<td>
+									<label className="swap text-sm">
+										<input type="checkbox" />
+										<div className="swap-on border rounded-full border-blue1 text-blue1 p-1 px-3">
+											09:00 - 12:00
+										</div>
+										<div className="swap-off border rounded-full border-gray text-gray p-1 px-3">
+											09:00 - 12:00
+										</div>
+									</label>
+								</td>
+								<td>
+									<label className="swap text-sm">
+										<input type="checkbox" />
+										<div className="swap-on border rounded-full border-blue1 text-blue1 p-1 px-3">
+											09:00 - 12:00
+										</div>
+										<div className="swap-off border rounded-full border-gray text-gray p-1 px-3">
+											09:00 - 12:00
+										</div>
+									</label>
+								</td>
+								<td>
+									<label className="swap text-sm">
+										<input type="checkbox" />
+										<div className="swap-on border rounded-full border-blue1 text-blue1 p-1 px-3">
+											09:00 - 12:00
+										</div>
+										<div className="swap-off border rounded-full border-gray text-gray p-1 px-3">
+											09:00 - 12:00
+										</div>
+									</label>
+								</td>
+							</tr>
+							{/* <!-- row 3 --> */}
+							<tr>
+								<th>
+									<img src={quadrant3} alt="quadrant" />
+								</th>
+								<td>
+									<label className="swap text-sm">
+										<input type="checkbox" />
+										<div className="swap-on border rounded-full border-blue1 text-blue1 p-1 px-3">
+											09:00 - 12:00
+										</div>
+										<div className="swap-off border rounded-full border-gray text-gray p-1 px-3">
+											09:00 - 12:00
+										</div>
+									</label>
+								</td>
+								<td>
+									<label className="swap text-sm">
+										<input type="checkbox" />
+										<div className="swap-on border rounded-full border-blue1 text-blue1 p-1 px-3">
+											09:00 - 12:00
+										</div>
+										<div className="swap-off border rounded-full border-gray text-gray p-1 px-3">
+											09:00 - 12:00
+										</div>
+									</label>
+								</td>
+								<td>
+									<label className="swap text-sm">
+										<input type="checkbox" />
+										<div className="swap-on border rounded-full border-blue1 text-blue1 p-1 px-3">
+											09:00 - 12:00
+										</div>
+										<div className="swap-off border rounded-full border-gray text-gray p-1 px-3">
+											09:00 - 12:00
+										</div>
+									</label>
+								</td>
+								<td>
+									<label className="swap text-sm">
+										<input type="checkbox" />
+										<div className="swap-on border rounded-full border-blue1 text-blue1 p-1 px-3">
+											09:00 - 12:00
+										</div>
+										<div className="swap-off border rounded-full border-gray text-gray p-1 px-3">
+											09:00 - 12:00
+										</div>
+									</label>
+								</td>
+								<td>
+									<label className="swap text-sm">
+										<input type="checkbox" />
+										<div className="swap-on border rounded-full border-blue1 text-blue1 p-1 px-3">
+											09:00 - 12:00
+										</div>
+										<div className="swap-off border rounded-full border-gray text-gray p-1 px-3">
+											09:00 - 12:00
+										</div>
+									</label>
+								</td>
+								<td>
+									<label className="swap text-sm">
+										<input type="checkbox" />
+										<div className="swap-on border rounded-full border-blue1 text-blue1 p-1 px-3">
+											09:00 - 12:00
+										</div>
+										<div className="swap-off border rounded-full border-gray text-gray p-1 px-3">
+											09:00 - 12:00
+										</div>
+									</label>
+								</td>
+								<td>
+									<label className="swap text-sm">
+										<input type="checkbox" />
+										<div className="swap-on border rounded-full border-blue1 text-blue1 p-1 px-3">
+											09:00 - 12:00
+										</div>
+										<div className="swap-off border rounded-full border-gray text-gray p-1 px-3">
+											09:00 - 12:00
+										</div>
+									</label>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default NewDoctorAddPage;
--- src/pages/polyclinic/doctors/PolyclinicDoctors.jsx
+++ src/pages/polyclinic/doctors/PolyclinicDoctors.jsx
@@ -1,10 +1,111 @@
-import React from 'react'
-import "./polyclinicDoctors.scss"
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import ClinicHeader from "../../../components/polyclinic/ClinicHeader";
+import Layout from "../../../components/polyclinic/Layout";
+import NewDoctorAddPage from "../../../components/polyclinic/doctors/NewDoctorAddPage";
+import { AiOutlineFileAdd } from "react-icons/ai";
 
-const PolyclinicDoctors = () => {
-  return (
-    <div>PolyclinicDoctors</div>
-  )
-}
+const PolyclinicAppointments = () => {
+	const open = useSelector((state) => state.open.value);
+	const [openTab, setOpenTab] = useState(1);
+	return (
+		<div>
+			<Layout />
 
-export default PolyclinicDoctors
+			<div
+				className={`absolute bg-container mt-20 right-0 p-6 min-h-[calc(100vh-80px)] ${
+					open ? "w-[calc(100%-250px)]" : "w-[calc(100%-100px)]"
+				} transition-w ease-in-out duration-500`}
+			>
+				{/* <----- Header -----> */}
+				<div className="bg-white">
+					<div className="divide-y p-1">
+						<ClinicHeader />
+						<div className="grid grid-cols-3 gap-3 border-gray"></div>
+					</div>
+					{/* personel title 👇🏻 */}
+					<div className="flex basis-4/6 justify-between items-center px-5 py-2">
+						<p className="text-2xl font-semibold antialiased leading-loose">
+							Personel İşlemleri
+						</p>
+						<button className="btn btn-active btn-sm bg-blue1 gap-2 rounded-full hover:bg-blue1 capitalize">
+							<AiOutlineFileAdd /> Doktor Ekle
+						</button>
+					</div>
+					<div className="px-5">
+						{/* <-----Tab Header ----->👇🏻 */}
+						<div className="tabs mb-5 ">
+							<p
+								className={` ${
+									openTab === 1
+										? "tab border-b-2 border-blue1 text-blue1 "
+										: "tab text-black"
+								}`}
+								onClick={(e) => {
+									e.preventDefault();
+									setOpenTab(1);
+								}}
+							>
+								Hekimlerimiz
+							</p>
+							<p
+								className={` ${
+									openTab === 2
+										? "tab border-b-2 border-blue1 text-blue1 "
+										: "tab text-black"
+								}`}
+								onClick={(e) => {
+									e.preventDefault();
+									setOpenTab(2);
+								}}
+							>
+								Performans
+							</p>
+							<p
+								className={` ${
+									openTab === 3
+										? "tab border-b-2 border-blue1 text-blue1 "
+										: "tab text-black"
+								}`}
+								onClick={(e) => {
+									e.preventDefault();
+									setOpenTab(3);
+								}}
+							>
+								İzinler
+							</p>
+							<p
+								className={`${
+									openTab === 4
+										? "tab border-b-2 border-blue1 text-blue1 "
+										: "tab text-black"
+								}`}
+								onClick={(e) => {
+									e.preventDefault();
+									setOpenTab(4);
+								}}
+							>
+								Yeni Kayıt
+							</p>
+						</div>
+						{/* <-----Tab Body ----->👇🏻 */}
+						<div className={openTab === 1 ? "block" : "hidden"}>
+							Hekimlerimiz Component gelecek
+						</div>
+						<div className={openTab === 2 ? "block" : "hidden"}>
+							Performans Component gelecek
+						</div>
+						<div className={openTab === 3 ? "block" : "hidden"}>
+							İzinler Component gelecek
+						</div>
+					</div>
+				</div>
+				<div className={openTab === 4 ? "block, bg-white" : "hidden"}>
+					<NewDoctorAddPage />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default PolyclinicAppointments;
