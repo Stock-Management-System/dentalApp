fe7f2984cb208d5c47524ba79d76dd28584d4a89
FatihG34 <fthgnydn34@gmail.com>
2022-11-25 11:21:35 +0300

    doctor ğages modified

--- src/pages/doctor/profile/DoctorProfile.jsx
+++ src/pages/doctor/profile/DoctorProfile.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
-import "./doctorProfile.scss"
+import { useSelector } from 'react-redux'
+import Layout from '../../../components/doctor/Layout'
+
 
 const DoctorProfile = () => {
+  const open = useSelector(state => state.open.value)
   return (
-    <div>DoctorProfile</div>
+    <>
+      <Layout />
+
+
+      <div
+        className={`absolute bg-container  mt-20 right-0 p-6 min-h-[calc(100vh-80px)] ${open ? 'w-[calc(100%-250px)]' : 'w-[calc(100%-100px)]'} transition-w ease-in-out duration-500`}
+      >
+        <div className="bg-white">
+          <div className='divide-y p-1'>
+            buraya ne gelecek
+            <br />
+            <hr />
+            Doktorun Banner i gelecek
+
+          </div>
+        </div>
+
+      </div>
+
+    </>
   )
 }
 
--- src/pages/doctor/settings/DoctorSettings.jsx
+++ src/pages/doctor/settings/DoctorSettings.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
-import "./doctorSettings.scss"
+import { useSelector } from 'react-redux'
+import Layout from '../../../components/doctor/Layout'
+
 
 const DoctorSettings = () => {
+  const open = useSelector(state => state.open.value)
   return (
-    <div>DoctorSettings</div>
+    <>
+      <Layout />
+
+
+      <div
+        className={`absolute bg-container  mt-20 right-0 p-6 min-h-[calc(100vh-80px)] ${open ? 'w-[calc(100%-250px)]' : 'w-[calc(100%-100px)]'} transition-w ease-in-out duration-500`}
+      >
+        <div className="bg-white">
+          <div className='divide-y p-1'>
+
+            Doktorun ayarlarrı
+
+          </div>
+        </div>
+
+      </div>
+
+    </>
   )
 }
 
