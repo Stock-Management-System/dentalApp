b25217aac11c2108c5756250485d1d116a8b3f37
FatihG34 <fthgnydn34@gmail.com>
2022-11-25 11:20:49 +0300

    doctors main structure modified

---
+++ src/components/doctor/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Sidebar from './Sidebar'
+
+
+const Layout = () => {
+    return (
+        <div>
+            <Navbar />
+            <Sidebar />
+        </div>
+    )
+}
+
+export default Layout
