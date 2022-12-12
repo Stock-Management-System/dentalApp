651ab468424d05ae54e54f70422128335a350f3a
FatihG34 <fthgnydn34@gmail.com>
2022-12-12 20:23:35 +0300

    modified

--- src/components/polyclinic/layout/NavBar.jsx
+++ src/components/polyclinic/layout/NavBar.jsx
@@ -7,6 +7,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { linkHoverToggle, sidebarToggle } from '../../../features/sidebarSlice';
 import AddAppointmentsModal from '../addAppointmentsModal/AddAppointmentsModal';
+import AddAppointmentsModalForXsNav from '../addAppointmentsModal/AddAppointmentsModalForXsNav';
 
 
 
@@ -41,7 +42,7 @@ const NavBar = () => {
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
           </label>
           <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-            <li><Link><BiMessageSquareAdd className='mr-1' /> Randevu Oluştur</Link></li>
+            <li><Link><BiMessageSquareAdd className='mr-1' /><AddAppointmentsModalForXsNav /></Link></li>
             <li><Link><BsCalendar3 className='mr-1' />Randevu Sorgula</Link></li>
           </ul>
         </div>
