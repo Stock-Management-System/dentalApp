12e8f3100abd92ae0f805574238d216d50c2b581
FatihG34 <fthgnydn34@gmail.com>
2022-12-13 19:48:05 +0300

    added modal Toggle state in store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import modalSlice from '../features/modalSlice'
 import openReducer from '../features/sidebarSlice'
 
 export const store = configureStore({
   reducer: {
     open: openReducer,
+    toggleM: modalSlice
   },
 })
