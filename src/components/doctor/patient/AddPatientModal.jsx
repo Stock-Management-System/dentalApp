4d440c0cea6fada98bf6a9c2789d07dca4529e7c
FatihG34 <fthgnydn34@gmail.com>
2022-12-27 22:33:57 +0300

    addpatient info states added

--- src/components/doctor/patient/AddPatientModal.jsx
+++ src/components/doctor/patient/AddPatientModal.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CgClose } from 'react-icons/cg'
+import { ilData } from '../../../helpers/ilData';
 
 const AddPatientModal = ({ setShowModal }) => {
+    const [addPatient, setAddPatient] = useState({
+        identity: "",
+        firstName: "",
+        lastName: "",
+        birthDay: "",
+        dadyName: "",
+        phoneNumber: "",
+        email: "",
+        city: ilData[0]["text"],
+        village: ilData[0]["districts"][0]["text"],
+    })
+
+    const handleChange = (e) => {
+        setAddPatient({ ...addPatient, [e.target.name]: e.target.value })
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setAddPatient({
+            identity: "",
+            firstName: "",
+            lastName: "",
+            birthDay: "",
+            dadyName: "",
+            phoneNumber: "",
+            email: "",
+            city: ilData[0]["text"],
+            village: ilData[0]["districts"][0]["text"],
+        })
+        setShowModal(false)
+    }
+    console.log(addPatient);
     return (
         <div>
             <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-blue2 bg-opacity-20">
@@ -17,25 +49,31 @@ const AddPatientModal = ({ setShowModal }) => {
                             </button>
                         </div>
                         <h1 className='ml-5 mt-3 font-semibold'>Hasta Bilgileri</h1>
-                        <form className="flex flex-col p-5 gap-2">
+                        <form className="flex flex-col p-5 gap-2" onSubmit={handleSubmit}>
                             <div className="md:flex justify-center items-center gap-3">
                                 <div>
                                     <label className="block label-text mb-1">
                                         TC KİMLİK NO
                                     </label>
-                                    <input className="input input-bordered input-sm w-52 xs:max-w-fit md:max-w-xs" />
+                                    <input type="text" name='identity' value={addPatient.identity}
+                                        onChange={handleChange}
+                                        className="input input-bordered input-sm w-52 xs:max-w-fit md:max-w-xs" />
                                 </div>
                                 <div>
                                     <label className="block label-text mb-1">
                                         DOĞUM TARİHİ
                                     </label>
-                                    <input className="input input-bordered input-sm w-52 xs:max-w-fit md:max-w-xs" />
+                                    <input type={"date"} name='birthDay' value={addPatient.birthDay}
+                                        onChange={handleChange}
+                                        className="input input-bordered input-sm w-52 xs:max-w-fit md:max-w-xs" />
                                 </div>
                                 <div>
                                     <label className="block label-text mb-1">
                                         EMAİL
                                     </label>
-                                    <input className="input input-bordered input-sm w-52 xs:max-w-fit md:max-w-xs" />
+                                    <input type={"email"} name='email' value={addPatient.email}
+                                        onChange={handleChange}
+                                        className="input input-bordered input-sm w-52 xs:max-w-fit md:max-w-xs" />
                                 </div>
                             </div>
                             <div className="md:flex justify-center items-center gap-3">
@@ -43,19 +81,31 @@ const AddPatientModal = ({ setShowModal }) => {
                                     <label className="block label-text mb-1">
                                         ADI
                                     </label>
-                                    <input className="input input-bordered input-sm w-52 xs:max-w-fit md:max-w-xs" />
+                                    <input type={"text"} name='firstName' value={addPatient.firstName}
+                                        onChange={handleChange}
+                                        className="input input-bordered input-sm w-52 xs:max-w-fit md:max-w-xs" />
                                 </div>
                                 <div>
                                     <label className="block label-text mb-1">
                                         BABA ADI
                                     </label>
-                                    <input className="input input-bordered input-sm w-52 xs:max-w-fit md:max-w-xs" />
+                                    <input type={"text"} name='dadyName' value={addPatient.dadyName}
+                                        onChange={handleChange}
+                                        className="input input-bordered input-sm w-52 xs:max-w-fit md:max-w-xs" />
                                 </div>
                                 <div>
                                     <label className="block label-text mb-1">
                                         İL
                                     </label>
-                                    <input className="input input-bordered input-sm w-52 xs:max-w-fit md:max-w-xs" />
+                                    <select
+                                        name="city"
+                                        id="city"
+                                        className="select select-sm select-bordered w-52 xs:max-w-fit md:max-w-xs"
+                                        value={addPatient.city}
+                                        onChange={handleChange}
+                                    >
+                                        {ilData.map((il) => <option value={il["text"]}>{il["text"]}</option>)}
+                                    </select>
                                 </div>
                             </div>
                             <div className="md:flex justify-center items-center gap-3">
@@ -63,19 +113,32 @@ const AddPatientModal = ({ setShowModal }) => {
                                     <label className="block label-text mb-1">
                                         SOYADI
                                     </label>
-                                    <input className="input input-bordered input-sm w-52 xs:max-w-fit md:max-w-xs" />
+                                    <input type={"text"} name='lastName' value={addPatient.lastName}
+                                        onChange={handleChange}
+                                        className="input input-bordered input-sm w-52 xs:max-w-fit md:max-w-xs" />
                                 </div>
                                 <div>
                                     <label className="block label-text mb-1">
                                         TELEFON
                                     </label>
-                                    <input className="input input-bordered input-sm w-52 xs:max-w-fit md:max-w-xs" />
+                                    <input type={"text"} name='phoneNumber' value={addPatient.phoneNumber}
+                                        onChange={handleChange}
+                                        className="input input-bordered input-sm w-52 xs:max-w-fit md:max-w-xs" />
                                 </div>
                                 <div>
                                     <label className="block label-text mb-1">
                                         İLÇE
                                     </label>
-                                    <input className="input input-bordered input-sm w-52 xs:max-w-fit md:max-w-xs" />
+                                    <select
+                                        name="village"
+                                        id="village"
+                                        className="select select-sm select-bordered w-52 xs:max-w-fit md:max-w-xs"
+                                        value={addPatient.village}
+                                        onChange={handleChange}
+                                    >
+                                        {ilData.filter((data) => (data["text"] === addPatient.city)).map((village) => (village["districts"].map((e) => <option value={e["text"]}>{e["text"]}</option>)))
+                                        }
+                                    </select>
                                 </div>
                             </div>
                             <div className="flex items-center justify-end mt-3">
@@ -88,8 +151,7 @@ const AddPatientModal = ({ setShowModal }) => {
                                 </button> */}
                                 <button
                                     className="btn btn-primary btn-sm w-40 uppercase text-sm rounded-full shadow hover:shadow-lg outline-none focus:outline-none"
-                                    type="button"
-                                    onClick={() => setShowModal(false)}
+                                    type="submit"
                                 >
                                     KAYDET
                                 </button>
