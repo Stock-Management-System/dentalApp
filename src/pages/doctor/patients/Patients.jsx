6b697d7f1bfce1e151f236940cb160a6ff55a563
FatihG34 <fthgnydn34@gmail.com>
2022-12-27 20:14:44 +0300

    query button and modal button added

--- src/pages/doctor/patients/Patients.jsx
+++ src/pages/doctor/patients/Patients.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { BsPersonPlus } from 'react-icons/bs'
+import { FaUserPlus } from 'react-icons/fa'
 import { useSelector } from 'react-redux'
+import DoctorHeader from '../../../components/doctor/doctorDashboard/DoctorHeader'
 import Layout from '../../../components/doctor/Layout'
+import AddPatientModal from '../../../components/doctor/patient/AddPatientModal'
+
 
 
 const Patients = () => {
+  const [showModal, setShowModal] = useState(false)
   const open = useSelector(state => state.open.value)
   return (
     <>
@@ -15,13 +21,35 @@ const Patients = () => {
       >
         <div className="bg-white">
           <div className='divide-y p-1'>
-            Doctors Patients
+            <DoctorHeader />
 
+          </div>
+        </div>
+        <div className='flex xs:flex-col md:flex-row items-center justify-between mt-7 mx-4 gap-6'>
+          <h1 className='text-28'>Hastalar</h1>
+          {/* <label className="label-text text-center">Hasta TC Kimlik No</label> */}
+          <div className='flex xs:flex-col md:flex-row items-center justify-between gap-4'>
+            <input
+              type="text"
+              placeholder="Hasta Ara ..."
+              className="input input-sm input-bordered w-40 max-w-xs rounded-3xl"
+            />
+            <select className="select select-bordered select-sm rounded-3xl max-w-xs">
+              <option disabled selected>Filtre Seçenekleri</option>
+              <option>Small Apple</option>
+              <option>Small Orange</option>
+              <option>Small Tomato</option>
+            </select>
+            <button className='btn btn-primary btn-sm rounded-full' onClick={() => setShowModal(true)}><BsPersonPlus className='mr-2 text-18' />Yeni Hasta Ekle</button>
           </div>
         </div>
 
       </div>
-
+      {
+        showModal
+        &&
+        <AddPatientModal setShowModal={setShowModal} />
+      }
     </>
   )
 }
