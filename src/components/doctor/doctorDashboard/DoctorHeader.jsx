380321d811f8030146f81b4e735392d1975db14a
selman <gunduzgece546@gmail.com>
2023-01-01 17:46:31 +0300

    dr profile added

--- src/components/doctor/doctorDashboard/DoctorHeader.jsx
+++ src/components/doctor/doctorDashboard/DoctorHeader.jsx
@@ -1,34 +1,37 @@
 import React from 'react'
 
 const DoctorHeader = () => {
-    return (
-        <div className="flex flex-row mt-16 gap-20 py-5">
-            {/* clinic header infos */}
-            <div className="flex basis-4/6 justify-start items-center px-10 py-4">
-                <img
-                    width={100}
-                    src="https://ozeldent42.com/tema/genel/uploads/hizmetler/icon/implant.png"
-                    alt="İnciDis"
-                />
-                <div className="text-2xl text-Sky-700 ml-8">
-                    <h1 className="font-bold" style={{ fontSize: "2rem" }}>
-                        İNCİ DİŞ
-                    </h1>
-                    <h2>POLİKLİNİĞİ</h2>
-                </div>
-            </div>
-            <div className="basis-2/12">
-                <h3>Adress</h3>
-                <p>Cumhuriyet Mahallesi Sağlık Caddesi Kat:2 No:5 FATİH/İSTANBUL</p>
-            </div>
-            <div className="basis-2/12">
-                <h3>Telefon</h3>
-                <p>+90 222 222 22 22</p>
-                <p>+90 222 222 22 22</p>
-                <p>+90 222 222 22 22</p>
-            </div>
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-12 flex-col gap-0 xl:gap-6 mt-16  py-5 justify-between">
+      {/* clinic header infos */}
+      <div className="flex justify-start items-center px-10 py-4 col-span-6">
+        <img
+          width={100}
+          src="https://ozeldent42.com/tema/genel/uploads/hizmetler/icon/implant.png"
+          alt="İnciDis"
+        />
+        <div className="text-2xl text-Sky-700 ml-8">
+          <h1 className="font-bold" style={{ fontSize: '2rem' }}>
+            İNCİ DİŞ
+          </h1>
+          <h2>POLİKLİNİĞİ</h2>
         </div>
-    )
+      </div>
+    
+        <div  className="xs:col-span-6 lg:col-span-3 p-6">
+          <h3 className="text-2xl font-bold">Adress :</h3>
+          <p>Cumhuriyet Mahallesi Sağlık Caddesi Kat:2 No:5</p>
+          <p className='text-end font-bold'> FATİH/İSTANBUL</p>
+        </div>
+        <div className="xs:col-span-6  lg:col-span-3 p-6">
+          <h3 className="text-2xl font-bold">Telefon :</h3>
+          <p>+90 222 222 22 22</p>
+          <p>+90 222 222 22 22</p>
+          <p>+90 222 222 22 22</p>
+        </div>
+   
+    </div>
+  )
 }
 
 export default DoctorHeader
--- src/pages/doctor/profile/DoctorProfile.jsx
+++ src/pages/doctor/profile/DoctorProfile.jsx
@@ -1,30 +1,50 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import DoctorHeader from '../../../components/doctor/doctorDashboard/DoctorHeader'
+import DoktorProfile from '../../../components/doctor/doctorDashboard/DoktorProfile'
 import Layout from '../../../components/doctor/Layout'
-
 
 const DoctorProfile = () => {
   const open = useSelector(state => state.open.value)
+  const doctorData =
+    {
+      id: 1,
+      name: 'Mehmet Ali',
+      surname: 'Kaya',
+      title: 'Ortodonti Uzmanı',
+      city: 'İstanbul',
+      district: 'Kadıköy',
+      address: 'İnci diş polikliniği',
+      phone: '0532 123 45 67',
+      email: 'malikaya@gmail.com',
+      graduateUniversity:'Selçuk Üniversitesi',
+      graduateYear: '12.07.2012',
+      workingSince: '20.09.2013',
+      workingDays:'Hafta içi hergün',
+      workingHours:'09:00-17:00',
+      profileImg: "https://down.imgspng.com/download/0720/doctor_PNG15959.png",
+
+    }
+
   return (
     <>
       <Layout />
 
-
       <div
-        className={`absolute bg-container  mt-20 right-0 p-6 min-h-[calc(100vh-80px)] ${open ? 'w-[calc(100%-250px)]' : 'w-[calc(100%-100px)]'} transition-w ease-in-out duration-500`}
+        className={`absolute bg-container  mt-20 right-0 p-6 min-h-[calc(100vh-80px)] ${
+          open ? 'w-[calc(100%-250px)]' : 'w-[calc(100%-100px)]'
+        } transition-w ease-in-out duration-500`}
       >
         <div className="bg-white">
-          <div className='divide-y p-1'>
-            buraya ne gelecek
-            <br />
-            <hr />
-            Doktorun Banner i gelecek
-
+          <div className="divide-y-2 divide-mainPageP p-1">
+            <DoctorHeader />
+            
+         
+            
+            <DoktorProfile doctorData={doctorData} />
           </div>
         </div>
-
       </div>
-
     </>
   )
 }
