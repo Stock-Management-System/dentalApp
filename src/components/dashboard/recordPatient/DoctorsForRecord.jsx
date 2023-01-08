d37351b55739a1bc0db20a2ad62a9e3329a6433e
FatihG34 <fthgnydn34@gmail.com>
2023-01-08 22:05:07 +0300

    card slider added

--- src/components/dashboard/recordPatient/DoctorsForRecord.jsx
+++ src/components/dashboard/recordPatient/DoctorsForRecord.jsx
@@ -1,23 +1,75 @@
 import React from 'react'
+import { useRef } from 'react';
+import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai';
+import Slider from 'react-slick';
 
 const DoctorsForRecord = () => {
-    const oylesine = [1, 2, 3, 4]
-    return (
-        <div className="carousel gap-4 p-5">
+    const oylesine = [1, 2, 3, 4, 5, 6, 7, 8];
+
+    const settings = {
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        initialSlide: 0,
+        centerMode: true,
+        centerPadding: '60px',
+        // draggable: true,
+        // focusOnSelect: true,
+        responsive: [
             {
-                oylesine.map((item, index) => (
-                    <div className="carousel-item" key={index}>
-                        <div className="card card-compact w-96 bg-base-100 p-1">
-                            <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-                            <div className='border-b opacity-80 w-[96%] my-1 mx-auto'></div>
-                            <div className="card-body">
-                                <h2 className="card-title text-blue1 font-normal">İNCİ DİŞ POLİKLİNİĞİ</h2>
-                            </div>
-                        </div>
-                    </div>
-                ))
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
             }
-        </div>
+        ]
+    };
+    const slider = useRef(null)
+    return (
+        <section className=''>
+            <article className='flex justify-between px-20'>
+                <h1>Doktorlarımız</h1>
+                <article className='flex gap-3'>
+                    <button onClick={() => slider?.current?.slickPrev()}><AiOutlineLeft className='text-28 bg-white rounded-full' /></button>
+                    <button onClick={() => slider?.current?.slickNext()}><AiOutlineRight className='text-28 bg-white rounded-full' /></button>
+                </article>
+            </article>
+            <div className="p-1 my-10">
+                <Slider ref={slider} {...settings}>
+                    {
+                        oylesine.map((item, index) => (
+                            <div key={index}>
+                                <div className="card card-compact w-96 bg-base-100 p-1" >
+                                    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                                    <div className='border-b opacity-80 w-[96%] my-1 mx-auto'></div>
+                                    <div className="card-body">
+                                        <h2 className="card-title text-blue1 font-normal">İNCİ DİŞ POLİKLİNİĞİ</h2>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </Slider>
+            </div>
+        </section>
     )
 }
 
--- src/components/dashboard/recordPatient/PolyclinicForRecord.jsx
+++ src/components/dashboard/recordPatient/PolyclinicForRecord.jsx
@@ -1,16 +1,57 @@
 import React from 'react';
 import { ilData } from "../../../helpers/ilData";
-import { SlArrowLeftCircle, SlArrowRightCircle } from 'react-icons/sl'
+import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick';
+import { useRef } from 'react';
 
 
 const PolyclinicForRecord = ({ place, setPlace }) => {
-    const oylesine = [1, 2, 3, 4]
+    const oylesine = [1, 2, 3, 4, 5, 6, 7, 8];
     const handleChange = (e) => {
         setPlace({ ...place, [e.target.name]: e.target.value })
-    }
+    };
+    const settings = {
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        initialSlide: 0,
+        centerMode: true,
+        centerPadding: '60px',
+        // draggable: true,
+        // focusOnSelect: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
+    const slider = useRef(null)
     return (
         <section>
-            <article className='flex justify-between'>
+            <article className='flex justify-between px-20'>
                 <h1>Poliklinik</h1>
                 <article className='flex gap-3'>
                     <select
@@ -34,23 +75,26 @@ const PolyclinicForRecord = ({ place, setPlace }) => {
                         {ilData.filter((data) => (data["text"] === place.city)).map((ilce) => (ilce["districts"].map((e) => <option value={e["text"]}>{e["text"]}</option>)))
                         }
                     </select>
-                    <button><SlArrowLeftCircle className='text-28 bg-white rounded-full' /></button><button><SlArrowRightCircle className='text-28 bg-white rounded-full' /></button>
+                    <button onClick={() => slider?.current?.slickPrev()}><AiOutlineLeft className='text-28 bg-white rounded-full' /></button>
+                    <button onClick={() => slider?.current?.slickNext()}><AiOutlineRight className='text-28 bg-white rounded-full' /></button>
                 </article>
             </article>
-            <div className="carousel gap-4 p-5">
-                {
-                    oylesine.map((item, index) => (
-                        <div className="carousel-item" key={index}>
-                            <div className="card card-compact w-96 bg-base-100 p-1">
-                                <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-                                <div className='border-b opacity-80 w-[96%] my-1 mx-auto'></div>
-                                <div className="card-body">
-                                    <h2 className="card-title text-blue1 font-normal">İNCİ DİŞ POLİKLİNİĞİ</h2>
+            <div className="p-1 my-10">
+                <Slider ref={slider} {...settings}>
+                    {
+                        oylesine.map((item, index) => (
+                            <div key={index}>
+                                <div className="card card-compact w-96 bg-base-100 p-1" >
+                                    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                                    <div className='border-b opacity-80 w-[96%] my-1 mx-auto'></div>
+                                    <div className="card-body">
+                                        <h2 className="card-title text-blue1 font-normal">İNCİ DİŞ POLİKLİNİĞİ</h2>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    ))
-                }
+                        ))
+                    }
+                </Slider>
             </div>
         </section>
     )
