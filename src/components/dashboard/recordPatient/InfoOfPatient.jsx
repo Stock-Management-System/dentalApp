e42db431d85103e0d1a97d3cd8552281a8727b4c
FatihG34 <fthgnydn34@gmail.com>
2023-01-09 17:02:03 +0300

    record patient section modified

--- src/components/dashboard/recordPatient/InfoOfPatient.jsx
+++ src/components/dashboard/recordPatient/InfoOfPatient.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { ilData } from '../../../helpers/ilData';
 
 const InfoOfPatient = () => {
+    const [personalInfo, setPersonalInfo] = useState({
+        firstName: "",
+        lastName: "",
+        dadyName: "",
+        birthDay: "",
+        phone: "",
+        email: "",
+        city: ilData[0]["text"],
+        town: ilData[0]["districts"][0]["text"]
+    });
+    const handleChange = (e) => {
+        setPersonalInfo({ ...personalInfo, [e.target.name]: e.target.value })
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // setPersonalInfo({
+        //     firstName: "",
+        //     lastName: "",
+        //     dadyName: "",
+        //     birthDay: "",
+        //     phone: "",
+        //     email: "",
+        //     city: ilData[0]["text"],
+        //     town: ilData[0]["districts"][0]["text"]
+        // })
+    }
     return (
-        <div>InfoOfPatient</div>
+        <section className='w-10/12 mx-auto px-1 py-5'>
+            <h1 className='text-22 my-8'>Hasta Bilgileri</h1>
+            <article className='grid xs:grid-rows-3 md:grid-cols-3 gap-5'>
+                <div>
+                    <p className='label-text'>Ad</p>
+                    <p className='input input-accent flex items-center'>Yunus</p>
+                </div>
+                <div>
+                    <p className='label-text'>Soyad</p>
+                    <p className='input input-accent flex items-center'>Alpaslan</p>
+                </div>
+                <div>
+                    <p className='label-text'>Telefon</p>
+                    <p className='input input-accent flex items-center'>0525 999 88 77</p>
+                </div>
+            </article>
+        </section>
     )
 }
 
---
+++ src/components/dashboard/recordPatient/InfoPatientForm.jsx
@@ -0,0 +1,156 @@
+import React, { useState } from 'react';
+import { ilData } from '../../../helpers/ilData';
+
+const InfoPatientForm = () => {
+    const [personalInfo, setPersonalInfo] = useState({
+        firstName: "",
+        lastName: "",
+        dadyName: "",
+        birthDay: "",
+        phone: "",
+        email: "",
+        city: ilData[0]["text"],
+        town: ilData[0]["districts"][0]["text"]
+    });
+    const handleChange = (e) => {
+        setPersonalInfo({ ...personalInfo, [e.target.name]: e.target.value })
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // setPersonalInfo({
+        //     firstName: "",
+        //     lastName: "",
+        //     dadyName: "",
+        //     birthDay: "",
+        //     phone: "",
+        //     email: "",
+        //     city: ilData[0]["text"],
+        //     town: ilData[0]["districts"][0]["text"]
+        // })
+    }
+    return (
+        <section className='w-10/12 mx-auto px-1 py-5 mt-8'>
+            <article>
+                <h3>Girilen TC kimlik numarasına ait bir kayıt bulunamadı. Lütfen Randevu almak için aşağıdaki formu doldurun.</h3>
+                <h1 className='text-22 mt-8 mb-4'>Hasta Bilgileri</h1>
+            </article>
+            <form >
+                <div className='grid xs:grid-cols-2 md:grid-cols-4 gap-10'>
+                    <div className="col-span-1">
+                        <label className="label">
+                            <span className="label-text">ADI<sup>*</sup></span>
+                        </label>
+                        <input
+                            type="text"
+                            name='firstName'
+                            value={personalInfo.firstName}
+                            onChange={handleChange}
+                            required
+                            placeholder="Hastanın Adı" className="input input-bordered input-sm focus:input-primary w-full max-w-xs" />
+                    </div>
+                    <div className="col-span-1">
+                        <label className="label">
+                            <span className="label-text">SOYADI<sup>*</sup></span>
+                        </label>
+                        <input
+                            type="text"
+                            name='lastName'
+                            value={personalInfo.lastName}
+                            onChange={handleChange}
+                            required
+                            placeholder="Hastanın Soyadı" className="input input-bordered input-sm focus:input-primary w-full max-w-xs" />
+                    </div>
+                    <div className="col-span-1">
+                        <label className="label">
+                            <span className="label-text">BABA ADI<sup>*</sup></span>
+                        </label>
+                        <input
+                            type="text"
+                            name='dadyName'
+                            value={personalInfo.dadyName}
+                            onChange={handleChange}
+                            required
+                            placeholder="Hastanın baba adı" className="input input-bordered input-sm focus:input-primary w-full max-w-xs" />
+                    </div>
+                    <div className="col-span-1">
+                        <label className="label">
+                            <span className="label-text">DOĞUM TARİHİ<sup>*</sup></span>
+                        </label>
+                        <input
+                            type="date"
+                            name='birthDay'
+                            value={personalInfo.birthDay}
+                            onChange={handleChange}
+                            required
+                            placeholder="Hastanın Doğum Tarihi" className="input input-bordered input-sm focus:input-primary w-full max-w-xs" />
+                    </div>
+                    <div className="col-span-1">
+                        <label className="label">
+                            <span className="label-text">TELEFON<sup>*</sup></span>
+                        </label>
+                        <input
+                            type="text"
+                            name='phone'
+                            value={personalInfo.phone}
+                            onChange={handleChange}
+                            required
+                            placeholder="Hastanın Telefon No" className="input input-bordered input-sm focus:input-primary w-full max-w-xs" />
+                    </div>
+                    <div className="col-span-1">
+                        <label className="label">
+                            <span className="label-text">EMAIL<sup>*</sup></span>
+                        </label>
+                        <input
+                            type="email"
+                            name='email'
+                            value={personalInfo.email}
+                            onChange={handleChange}
+                            required
+                            placeholder="abcd@abcd.com" className="input input-bordered input-sm focus:input-primary w-full max-w-xs" />
+                    </div>
+                    <div className="col-span-1">
+                        <label className="label">
+                            <span className="label-text">İL<sup>*</sup></span>
+                        </label>
+                        <select
+                            name='city'
+                            value={personalInfo.city}
+                            onChange={handleChange}
+                            required
+                            className="select select-bordered select-sm text-14 font-normal focus:border-blue1 w-full max-w-xs"
+                        >
+                            {ilData.map((il) => <option value={il["text"]}>{il["text"]}</option>)}
+                        </select>
+                    </div>
+                    <div className="col-span-1">
+                        <label className="label">
+                            <span className="label-text">İLÇE<sup>*</sup></span>
+                        </label>
+                        <select
+                            name='town'
+                            value={personalInfo.town}
+                            onChange={handleChange}
+                            required
+                            className=" select select-bordered select-sm text-14 font-normal focus:border-blue1 w-full max-w-xs"
+                        >
+                            {ilData.filter((data) => (data["text"] === personalInfo.city)).map((ilce) => (ilce["districts"].map((e) => <option value={e["text"]}>{e["text"]}</option>)))
+                            }
+                        </select>
+                    </div>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end mx-4 mt-4 p-2 rounded-b">
+                    <button
+                        className="bg-blue1 text-white capitalize btn btn-sm rounded-3xl hover:bg-blue1"
+                        type="submit"
+                        onClick={handleSubmit}
+                    >
+                        Bilgileri Kaydet
+                    </button>
+                </div>
+            </form>
+        </section>
+    )
+}
+
+export default InfoPatientForm
