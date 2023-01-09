827fa9fa64c1b790e8967249712f161b79364c3b
FatihG34 <fthgnydn34@gmail.com>
2023-01-09 13:27:20 +0300

    Slider removed, swiper added for doctors cards

--- src/components/dashboard/recordPatient/DoctorsForRecord.jsx
+++ src/components/dashboard/recordPatient/DoctorsForRecord.jsx
@@ -1,73 +1,98 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRef } from 'react';
 import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai';
-import Slider from 'react-slick';
+import { Navigation, Pagination, Scrollbar, A11y, Controller, Autoplay } from "swiper";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { doktorlar } from "../../../helpers/data";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+// import 'swiper/css/effect-fade';
 
 const DoctorsForRecord = () => {
-    const oylesine = [1, 2, 3, 4, 5, 6, 7, 8];
+    const [openTab, setOpenTab] = useState(0);
+    const [swiper, setSwiper] = useState();
+    const prevRef = useRef();
+    const nextRef = useRef();
+    useEffect(() => {
+        if (swiper) {
+            console.log("Swiper instance:", swiper);
+            swiper.params.navigation.prevEl = prevRef.current;
+            swiper.params.navigation.nextEl = nextRef.current;
+            swiper.navigation.init();
+            swiper.navigation.update();
+        }
+    }, [swiper]);
 
-    const settings = {
-        infinite: true,
-        speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        initialSlide: 0,
-        centerMode: true,
-        centerPadding: '60px',
-        // draggable: true,
-        // focusOnSelect: true,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                    initialSlide: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    };
-    const slider = useRef(null)
     return (
         <section className=''>
             <article className='flex justify-between px-20'>
                 <h1>Doktorlarımız</h1>
                 <article className='flex gap-3'>
-                    <button onClick={() => slider?.current?.slickPrev()}><AiOutlineLeft className='text-28 bg-white rounded-full' /></button>
-                    <button onClick={() => slider?.current?.slickNext()}><AiOutlineRight className='text-28 bg-white rounded-full' /></button>
+                    <button ref={prevRef}><AiOutlineLeft className='text-28 bg-white rounded-full' /></button>
+                    <button ref={nextRef}><AiOutlineRight className='text-28 bg-white rounded-full' /></button>
                 </article>
             </article>
             <div className="p-1 my-10">
-                <Slider ref={slider} {...settings}>
-                    {
-                        oylesine.map((item, index) => (
-                            <div key={index}>
-                                <div className="card card-compact w-96 bg-base-100 p-1" >
-                                    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-                                    <div className='border-b opacity-80 w-[96%] my-1 mx-auto'></div>
-                                    <div className="card-body">
-                                        <h2 className="card-title text-blue1 font-normal">İNCİ DİŞ POLİKLİNİĞİ</h2>
-                                    </div>
-                                </div>
+                {/* <Slider ref={slider} {...settings}> */}
+
+
+                <Swiper
+                    className="flex flex-row gap-5"
+                    spaceBetween={25}
+                    // slidesPerView="auto"
+                    slidesPerView={5}
+                    onSlideChange={() => console.log("slide change")}
+                    onSwiper={setSwiper}
+                    modules={[Navigation, Pagination, Scrollbar, A11y, Controller, Autoplay]}
+                    navigation={{
+                        prevEl: prevRef?.current,
+                        nextEl: nextRef?.current,
+                    }}
+                    autoplay={{
+                        delay: 1500,
+                        disableOnInteraction: false
+                    }}
+                    loop={true}
+                    updateOnWindowResize
+                    observer
+                    observeParents
+                >
+                    {doktorlar.map((doktor, index) => (
+                        <SwiperSlide
+                            className={` ${openTab === doktor.id
+                                ? "card w-1/5 bg-gradient-to-r  from-blue1 to-blue2 text-white"
+                                : "card w-1/5 bg-white"
+                                }`}
+                            onClick={(e) => {
+                                e.preventDefault();
+                                setOpenTab(doktor.id);
+                            }}
+                        >
+                            <figure className="pt-5">
+                                <img
+                                    src={doktor.img}
+                                    alt={doktor.name}
+                                    className="w-36 h-36 rounded-full object-contain"
+                                />
+                            </figure>
+                            <div className="text-center py-3">
+                                <h2 className="text-center text-xl font-semibold">
+                                    {doktor.name}
+                                </h2>
+                                <p
+                                    className={` ${openTab === doktor.id
+                                        ? "text-white text-center"
+                                        : "text-blue1 text-center"
+                                        }`}
+                                >
+                                    {doktor.ünvan}
+                                </p>
                             </div>
-                        ))
-                    }
-                </Slider>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
             </div>
         </section>
     )
