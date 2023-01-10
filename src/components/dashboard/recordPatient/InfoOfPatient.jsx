167c5f40008baaa9a4ea0757c4378fa527ad798e
FatihG34 <fthgnydn34@gmail.com>
2023-01-10 11:51:30 +0300

    modified

--- src/components/dashboard/recordPatient/InfoOfPatient.jsx
+++ src/components/dashboard/recordPatient/InfoOfPatient.jsx
@@ -1,48 +1,23 @@
 import React, { useState } from 'react';
 import { ilData } from '../../../helpers/ilData';
 
-const InfoOfPatient = () => {
-    const [personalInfo, setPersonalInfo] = useState({
-        firstName: "",
-        lastName: "",
-        dadyName: "",
-        birthDay: "",
-        phone: "",
-        email: "",
-        city: ilData[0]["text"],
-        town: ilData[0]["districts"][0]["text"]
-    });
-    const handleChange = (e) => {
-        setPersonalInfo({ ...personalInfo, [e.target.name]: e.target.value })
-    }
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // setPersonalInfo({
-        //     firstName: "",
-        //     lastName: "",
-        //     dadyName: "",
-        //     birthDay: "",
-        //     phone: "",
-        //     email: "",
-        //     city: ilData[0]["text"],
-        //     town: ilData[0]["districts"][0]["text"]
-        // })
-    }
+const InfoOfPatient = ({ patientInfo }) => {
+
     return (
         <section className='w-10/12 mx-auto'>
-            <h1 className='text-22 my-8'>Hasta Bilgileri</h1>
+            <h1 className='text-22 my-8 mx-auto'>Hasta Bilgileri</h1>
             <article className='grid xs:grid-cols-1 md:grid-cols-3 gap-5'>
-                <div className=''>
-                    <p className='label-text'>Ad</p>
-                    <p className='input input-accent flex items-center'>Yunus</p>
+                <div className='w-9/12 mx-auto'>
+                    <p className='label-text ml-4 mb-1'>Ad</p>
+                    <p className='input xs:input-sm md:input-md input-accent flex items-center'>{patientInfo.firstName}</p>
                 </div>
-                <div className=''>
-                    <p className='label-text'>Soyad</p>
-                    <p className='input input-accent flex items-center'>Alpaslan</p>
+                <div className='w-9/12 mx-auto'>
+                    <p className='label-text ml-4 mb-1'>Soyad</p>
+                    <p className='input xs:input-sm md:input-md input-accent flex items-center'>{patientInfo.lastName}</p>
                 </div>
-                <div className=''>
-                    <p className='label-text'>Telefon</p>
-                    <p className='input input-accent flex items-center'>0525 999 88 77</p>
+                <div className='w-9/12 mx-auto'>
+                    <p className='label-text ml-4 mb-1'>Telefon</p>
+                    <p className='input xs:input-sm md:input-md input-accent flex items-center'>{patientInfo.phone}</p>
                 </div>
             </article>
         </section>
