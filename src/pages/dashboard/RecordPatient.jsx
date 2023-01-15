bcc25c6a00b5c6e24fffa01d2bf90404e0bbffa6
FatihG34 <fthgnydn34@gmail.com>
2023-01-16 00:09:59 +0300

    modified

--- src/pages/dashboard/RecordPatient.jsx
+++ src/pages/dashboard/RecordPatient.jsx
@@ -9,7 +9,6 @@ import InfoOfPatient from '../../components/dashboard/recordPatient/InfoOfPatien
 import InfoPatientForm from '../../components/dashboard/recordPatient/InfoPatientForm';
 import PolyclinicForRecord from '../../components/dashboard/recordPatient/PolyclinicForRecord';
 import RegisterModal from '../../components/dashboard/RegisterModal';
-import { ilData } from "../../helpers/ilData";
 import InciDıs from '../../assets/InciDis.png';
 const patient = [
     {
@@ -35,7 +34,8 @@ const patient = [
         town: "Mengen"
     },
 ]
-
+const date1 = new Date()
+const thisDay = date1.toISOString()
 
 const RecordPatient = () => {
     const [showRegister, setShowRegister] = useState(false);
@@ -43,7 +43,17 @@ const RecordPatient = () => {
     const [showForgotPassword, setShowForgotPassword] = useState(false);
     const [showInfos, setShowInfos] = useState(false)
     const [queryTC, setQueryTC] = useState("")
-    const [patientInfo, setPatientInfo] = useState("")
+    const [patientInfo, setPatientInfo] = useState({  //* bu state ler kayıtlı olmayan hastaların bilgilerinin kayıt edilmesi için de tutulan state ler
+        TC: queryTC,
+        firstName: "",
+        lastName: "",
+        dadyName: "",
+        birthDay: "",
+        phone: "",
+        email: "",
+        city: "",
+        town: ""
+    })
     const [polyclinic, setPolyclinic] = useState({
         polyclinicId: 1,
         polyclinicName: 'Has İnci Diş Polikliniği',
@@ -60,41 +70,30 @@ const RecordPatient = () => {
         doctorPhoto: "https://www.seekpng.com/png/full/855-8557328_headshot-of-dr-male-dentist-png.png"
     })
     const [clock, setClock] = useState("");
-    const [date, setDate] = useState("")
+    const [date, setDate] = useState(thisDay.slice(8, 10) + "." + thisDay.slice(5, 7) + "." + thisDay.slice(0, 4))
     const [place, setPlace] = useState({ city: "İstanbul", town: "Avcılar" })
-    const [personalInfo, setPersonalInfo] = useState({  //* bu state ler kayıtlı olmayan hastaların bilgilerinin kayıt edilmesi için tutulan state ler
-        TC: queryTC,
-        firstName: "",
-        lastName: "",
-        dadyName: "",
-        birthDay: "",
-        phone: "",
-        email: "",
-        city: ilData[0]["text"],
-        town: ilData[0]["districts"][0]["text"]
-    });
+
 
     const filteredPatient = patient.filter((item) => item.TC === queryTC)
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        setPatientInfo((filteredPatient[0]?.TC !== "") && filteredPatient[0])
+        setPatientInfo(filteredPatient[0])
         setShowInfos(true)
     }
-    // console.log(queryTC)
-    // console.log("Filter lanmış veri :", filteredPatient[0]?.TC)
-    // console.log("Patient Info :", patientInfo)
-    // console.log("Patient Info nun TC si :", patientInfo?.TC)
+
     return (
         <div className='h-full'>
             <NavBar setShowRegister={setShowRegister} />
             <section className='pt-28'>
                 {
                     !showInfos
-                    &&
-                    <div className='flex items-center justify-center p-10 text-2xl mb-10'>
-                        <h1>Lütfen Randevu Almak için TC Kimlik numaranız ile kaydınızı kontrol ediniz.</h1>
-                    </div>
+                        ?
+                        <div className='flex items-center justify-center p-10 text-2xl mb-10'>
+                            <h1>Lütfen Randevu Almak için TC Kimlik numaranız ile kaydınızı kontrol ediniz.</h1>
+                        </div>
+                        :
+                        null
                 }
                 <article className={`flex xs:flex-col md:flex-row xs:gap-2 md:gap-0 md:justify-between w-10/12 mx-auto`} >
                     <h1 className='text-3xl text-blue1'>Randevu Al</h1>
@@ -116,7 +115,7 @@ const RecordPatient = () => {
                             ?
                             <InfoOfPatient patientInfo={patientInfo} />
                             :
-                            <InfoPatientForm queryTC={queryTC} setPersonalInfo={setPersonalInfo} personalInfo={personalInfo} />
+                            <InfoPatientForm queryTC={queryTC} setPersonalInfo={setPatientInfo} personalInfo={patientInfo} />
                 }
             </section>
             <section className=''>
@@ -129,7 +128,7 @@ const RecordPatient = () => {
 
                 <article className="w-11/12 mx-auto my-8">
                     <article className=''>
-                        <h5>Sayın <strong>{patientInfo.firstName} {patientInfo.lastName}</strong> . Randevunuz aşağıdaki şekilde oluşturulacaktır.</h5>
+                        <h5>Sayın <strong>{patientInfo?.firstName !== undefined ? patientInfo?.firstName : "..."} {patientInfo?.lastName !== undefined ? patientInfo?.lastName : "..."}</strong> . Randevunuz aşağıdaki şekilde oluşturulacaktır.</h5>
                         <div className='my-5'>
                             <h4 className='opacity-80'>POLİKLİNİK</h4>
                             <h6 className='font-semibold'>{polyclinic.polyclinicName}</h6>
@@ -140,7 +139,7 @@ const RecordPatient = () => {
                         </div>
                         <div className='my-5'>
                             <h4 className='opacity-80'>TARİH / SAAT</h4>
-                            <h6 className='font-semibold'>{date.slice(8, 10)}.{date.slice(5, 7)}.{date.slice(0, 4)} / {clock}</h6>
+                            <h6 className='font-semibold'>{date} / {clock}</h6>
                         </div>
                     </article>
                     <button
